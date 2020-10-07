44d06d6cf85bb4dd8c1514b92b32c950bada9ac1
jianlinwei <jianlinwei_dream@163.com>
2020-10-07 16:43:40 +0800

    人员表格

--- src/pages/persons/list/index.vue
+++ src/pages/persons/list/index.vue
@@ -1,3 +1,181 @@
 <template>
-  <div>ddd</div>
+  <a-card>
+    <div :class="advanced ? 'search' : null">
+      <a-form layout="horizontal">
+        <div :class="advanced ? null : 'fold'">
+          <a-row>
+            <a-col :md="8" :sm="24">
+              <a-form-item
+                label="ID"
+                :labelCol="{ span: 5 }"
+                :wrapperCol="{ span: 18, offset: 1 }"
+              >
+                <a-input placeholder="请输入" />
+              </a-form-item>
+            </a-col>
+            <a-col :md="8" :sm="24">
+              <a-form-item
+                label="姓名"
+                :labelCol="{ span: 5 }"
+                :wrapperCol="{ span: 18, offset: 1 }"
+              >
+                <a-input placeholder="请输入" />
+              </a-form-item>
+            </a-col>
+            <a-col :md="8" :sm="24">
+              <a-form-item
+                label="身份证号"
+                :labelCol="{ span: 5 }"
+                :wrapperCol="{ span: 18, offset: 1 }"
+              >
+                <a-input placeholder="请输入" />
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row v-if="advanced">
+            <a-col :md="8" :sm="24">
+              <a-form-item
+                label="门禁卡号"
+                :labelCol="{ span: 5 }"
+                :wrapperCol="{ span: 18, offset: 1 }"
+              >
+                <a-input placeholder="请输入" />
+              </a-form-item>
+            </a-col>
+            <a-col :md="8" :sm="24">
+              <a-form-item
+                label="角色"
+                :labelCol="{ span: 5 }"
+                :wrapperCol="{ span: 18, offset: 1 }"
+              >
+                <a-select placeholder="请选择">
+                  <a-select-option value="1"> 普通人员</a-select-option>
+                  <a-select-option value="2"> 白名单人员</a-select-option>
+                  <a-select-option value="3"> 黑名单人员</a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+          </a-row>
+        </div>
+        <span style="float: right; margin-top: 3px">
+          <a-button type="primary">查询</a-button>
+          <a-button style="margin-left: 8px">重置</a-button>
+          <a @click="toggleAdvanced" style="margin-left: 8px">
+            {{ advanced ? "收起" : "展开" }}
+            <a-icon :type="advanced ? 'up' : 'down'" />
+          </a>
+        </span>
+      </a-form>
+    </div>
+    <div>
+      <div class="operator">
+       
+        <a-button ghost type="danger">批量删除</a-button>
+       
+      </div>
+      <standard-table
+        :dataSource="dataSource"
+        :selectedRows.sync="selectedRows"
+        @clear="onClear"
+        @change="onChange"
+        @selectedRowChange="onSelectChange"
+      >
+      
+        <div slot="action" slot-scope="{record }">
+         
+          <a style="margin-right: 8px"> <a-icon type="edit" />编辑 </a>
+          <a  @click="deleteRecord(record.key)"   >
+            <a-icon type="delete" />删除
+          </a>
+         
+        </div>
+        <template slot="statusTitle">
+          <a-icon @click.native="onStatusTitleClick" type="info-circle" />
+        </template>
+      </standard-table>
+    </div>
+  </a-card>
 </template>
+
+<script>
+import StandardTable from "./table/StandardTable";
+
+
+
+
+export default {
+  name: "QueryList",
+  components: { StandardTable },
+  data() {
+    return {
+      advanced: true,
+      dataSource: [],
+      selectedRows: [],
+    };
+  },
+  authorize: {
+    deleteRecord: "delete",
+  },
+  methods: {
+    deleteRecord(key) {
+      this.dataSource = this.dataSource.filter((item) => item.key !== key);
+      this.selectedRows = this.selectedRows.filter((item) => item.key !== key);
+    },
+    toggleAdvanced() {
+      this.advanced = !this.advanced;
+    },
+    remove() {
+      this.dataSource = this.dataSource.filter(
+        (item) =>
+          this.selectedRows.findIndex((row) => row.key === item.key) === -1
+      );
+      this.selectedRows = [];
+    },
+    onClear() {
+      this.$message.info("您清空了勾选的所有行");
+    },
+    onStatusTitleClick() {
+      this.$message.info("你点击了状态栏表头");
+    },
+    onChange() {
+      this.$message.info("表格状态改变了");
+    },
+    onSelectChange() {
+      this.$message.info("选中行改变了");
+    },
+    addNew() {
+      this.dataSource.unshift({
+        key: this.dataSource.length,
+        no: "NO " + this.dataSource.length,
+        description: "这是一段描述",
+        callNo: Math.floor(Math.random() * 1000),
+        status: Math.floor(Math.random() * 10) % 4,
+        updatedAt: "2018-07-26",
+      });
+    },
+    handleMenuClick(e) {
+      if (e.key === "delete") {
+        this.remove();
+      }
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.search {
+  margin-bottom: 54px;
+}
+.fold {
+  width: calc(100% - 216px);
+  display: inline-block;
+}
+.operator {
+  margin-bottom: 18px;
+}
+@media screen and (max-width: 900px) {
+  .fold {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/pages/persons/list/table/StandardTable.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="standard-table">
+    
+    <a-table
+      :bordered="bordered"
+      :loading="loading"
+      :columns="columns"
+      :dataSource="dataSource"
+      :rowKey="rowKey"
+      :pagination="pagination"
+      :expandedRowKeys="expandedRowKeys"
+      :expandedRowRender="expandedRowRender"
+      @change="onChange"
+      :rowSelection="selectedRows ? {selectedRowKeys: selectedRowKeys, onChange: updateSelect} : undefined"
+    >
+      <template slot-scope="text, record, index" :slot="slot" v-for="slot in Object.keys($scopedSlots).filter(key => key !== 'expandedRowRender') ">
+        <slot :name="slot" v-bind="{text, record, index}"></slot>
+      </template>
+      <template :slot="slot" v-for="slot in Object.keys($slots)">
+        <slot :name="slot"></slot>
+      </template>
+      <template slot-scope="record, index, indent, expanded" :slot="$scopedSlots.expandedRowRender ? 'expandedRowRender' : ''">
+        <slot v-bind="{record, index, indent, expanded}" :name="$scopedSlots.expandedRowRender ? 'expandedRowRender' : ''"></slot>
+      </template>
+    </a-table>
+  </div>
+</template>
+
+<script>
+import {queryPersonsList} from "@/services/person"
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+  },
+  {
+    title: "姓名",
+    dataIndex: "name",
+    scopedSlots: { customRender: "name" },
+  },
+  {
+    title: "身份证号",
+    dataIndex: "idCard",
+  },
+ {
+    title: "门禁卡号",
+    dataIndex: "accessId",
+    sorter: true
+  },
+  {
+    title: "车牌号",
+    dataIndex: "carId",
+  },
+  {
+    title: "电话",
+    dataIndex: "phone",
+  },
+  {
+    title: "照片",
+    dataIndex: "photo",
+  },
+  {
+    title: "角色",
+    dataIndex: "role",
+  },
+  {
+    title: "生效时间",
+    dataIndex: "starTime",
+    sorter: true
+  },
+  {
+    title: "失效时间",
+    dataIndex: "invalidTime",
+    sorter: true
+  },
+  {
+    title: "注册时间",
+    dataIndex: "createTime",
+    sorter: true
+  },
+  {
+    title: "备注",
+    dataIndex: "content",
+  },
+  
+  {
+    title: "操作",
+    scopedSlots: { customRender: "action" },
+  },
+];
+export default {
+  name: 'StandardTable',
+  props: {
+    bordered: Boolean,
+    loading: [Boolean, Object],
+    
+    dataSource: Array,
+    rowKey: {
+      type: [String, Function],
+      default: 'key'
+    },
+    pagination: {
+      type: [Object, Boolean],
+      default: true
+    },
+    selectedRows: Array,
+    expandedRowKeys: Array,
+    expandedRowRender: Function
+  },
+  data () {
+    return {
+      needTotalList: [],
+      columns: columns,
+      page:{
+        page:1,
+        limit:10
+      }
+      
+    }
+  },
+  methods: {
+    queryPersonsList(){
+      queryPersonsList(this.page).then(res =>{
+        console.log(res)
+      })
+    },
+    updateSelect (selectedRowKeys, selectedRows) {
+      this.$emit('update:selectedRows', selectedRows)
+      this.$emit('selectedRowChange', selectedRowKeys, selectedRows)
+    },
+    initTotalList (columns) {
+      const totalList = columns.filter(item => item.needTotal)
+        .map(item => {
+          return {
+            ...item,
+            total: 0
+          }
+        })
+      return totalList
+    },
+    onClear() {
+      this.updateSelect([], [])
+      this.$emit('clear')
+    },
+    onChange(pagination, filters, sorter, {currentDataSource}) {
+      this.$emit('change', pagination, filters, sorter, {currentDataSource})
+    }
+  },
+  created () {
+   // this.needTotalList = this.initTotalList(this.columns)
+   this.queryPersonsList()
+  },
+  watch: {
+    selectedRows (selectedRows) {
+      this.needTotalList = this.needTotalList.map(item => {
+        return {
+          ...item,
+          total: selectedRows.reduce((sum, val) => {
+            return sum + val[item.dataIndex]
+          }, 0)
+        }
+      })
+    }
+  },
+  computed: {
+    selectedRowKeys() {
+      return this.selectedRows.map(record => {
+        return (typeof this.rowKey === 'function') ? this.rowKey(record) : record[this.rowKey]
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.standard-table{
+  .alert{
+    margin-bottom: 16px;
+    .message{
+      a{
+        font-weight: 600;
+      }
+    }
+    .clear{
+      float: right;
+    }
+  }
+}
+</style>
